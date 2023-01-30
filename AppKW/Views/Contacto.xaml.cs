edb773c2da83fa7247453bf26b575c109f2427bc
Cristian <practicas.ti.kw@gmail.com>
2023-01-30 11:56:30 -0600

    Prueba con firebase realtime

--- AppKW/Views/Contacto.xaml.cs
+++ AppKW/Views/Contacto.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AppKW.Models;
+using AppKW.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,54 @@ namespace AppKW.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Contacto : ContentPage
     {
+        readonly FirebaseHelper firebaseHelper = new FirebaseHelper();
         public Contacto()
         {
             InitializeComponent();
         }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            await FetchAllPosts();
+        }
+
+        private async void BtnAdd_Clicked(object sender, EventArgs e)
+        {
+            if (!IsFormValid())
+            {
+                await DisplayAlert("Error", "Title are required", "OK");
+                return;
+            }
+
+            await firebaseHelper.AddPost(TxtTitle.Text);
+
+            TxtTitle.Text = string.Empty;
+
+            await DisplayAlert("Success", "Post Added Successfully", "OK");
+
+            await FetchAllPosts();
+        }
+
+        private async void LstPosts_OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var post = await firebaseHelper.GetPost(SelectedPost.PostId);
+
+            TxtTitle.Text = post.Title;
+        }
+
+        private async Task FetchAllPosts()
+        {
+            var allPersons = await firebaseHelper.GetAllPosts();
+
+            LstPosts.ItemsSource = allPersons;
+        }
+
+        private Post SelectedPost => (Post)LstPosts.SelectedItem;
+
+        private bool IsFormValid() => IsTitleValid();
+
+        private bool IsTitleValid() => !string.IsNullOrWhiteSpace(TxtTitle.Text);
     }
 }
