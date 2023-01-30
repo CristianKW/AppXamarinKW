82ed05354529754b05756e16bc2f12ffd38c9727
Magdalena Pozos <kwpracticas@gmail.com>
2023-01-30 17:11:34 -0600

    Cambios en las paginas de Inicio y Buzon.

--- AppKW/Views/Inicio.xaml.cs
+++ AppKW/Views/Inicio.xaml.cs
@@ -18,5 +18,19 @@ namespace AppKW.Views
 
 
         }
+
+        async void Button_VisionClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("NUESTRA VISIÓN", "Consolidarnos como una empresa líder, " +
+                "rentable, en constante crecimiento, comprometidos con la calidad, el desarrollo " +
+                "de nuestro personal y de la comunidad", "Ok");
+        }
+
+        async void Button_MisionClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("NUESTRA MISIÓN", "Somos una organización en constante desarrollo, " +
+                "comprometidos en dar solución a los retos del transporte, superando las expectativas " +
+                "de nuestros clientes, fortaleciendo nuestra empresa y comunidad.", "Ok");
+        }
     }
 }
