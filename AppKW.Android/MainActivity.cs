9c98ab07622038873f041b153f5262237f7fc12a
Magdalena Pozos <kwpracticas@gmail.com>
2023-02-13 11:45:11 -0600

    Cambio de icono de la aplicacion KW.

--- AppKW.Android/MainActivity.cs
+++ AppKW.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace AppKW.Droid
 {
-    [Activity(Label = "AppKW", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "AppKW", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
